6121e4f050eec50d5d34bee713fa9af139d73547
Galiia Uzbekova <galiiauzbekova@gmail.com>
2021-12-08 12:09:48 +0500

    added api

---
+++ js/service.js
@@ -0,0 +1,20 @@
+const API_KEY = 'db9ee4f567b29efba2bdce1a2d5bcd54';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+// trending/all/day?api_key=<<api_key>>
+
+const getData = url => fetch(url)
+      .then(response => {
+            if (response.ok) {
+                  return response.json()
+            }
+            throw `Что-то пошло не так, ошибка ${response.status}`
+      })
+      .catch(err => console.error(err));
+
+
+export const getTrends = async ( type = 'all', period = 'week', page = 1) => {
+      const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+      return await getData(url)
+};
